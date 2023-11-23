8ddc41cfaf084e941797e5d333a6bf7b85901f59
Eldar Safin <git@eldar-s.online>
2023-11-23 13:58:45 +0300

    Добавил журналирование

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 
+import logging
+import os
 import sys
 
 
+log_level: str = os.getenv('LOG_LEVEL', 'INFO')
+logging.basicConfig(
+    level=logging._nameToLevel[log_level]
+    )
+
+
 def main():
+    logging.info('main()')
     return 0
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     sys.exit(main())
 
