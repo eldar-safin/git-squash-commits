9414ed12bcb773cfa8b32683e0a2355e140e05c6
Eldar Safin <git@eldar-s.online>
2023-11-23 17:39:49 +0300

    Перенес настройки в settings.py. Добавил исключение ConnectionError

--- main.py
+++ main.py
@@ -6,33 +6,32 @@ import requests
 import sys
 from typing import List
 
+from settings import settings
+
 # Change the current working directory to the directory where this script resides
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-log_level: str = os.getenv('LOG_LEVEL', 'INFO')
-
 logging.basicConfig(
     format='%(asctime)s | %(filename)16s:%(lineno)-4d | %(levelname)-8s | %(message)s',
-    level=logging._nameToLevel[log_level]
+    level=logging._nameToLevel[settings.log.level]
 )
 
-urls_filename: str = os.getenv('URLS_FILENAME', 'urls.txt')
 urls_list: List[str] = list(
-    map(lambda line: line.strip(), open(urls_filename).readlines()))
+    map(lambda line: line.strip(), open(settings.urls.filename).readlines()))
 
 
 def main() -> int:
     for url_item in urls_list:
         logging.info(f'Check {url_item}')
 
-        def url_exist(url: str) -> bool:
+        def check_url(url: str) -> bool:
             try:
                 response: requests.Response = requests.get(url, timeout=3)
                 return response.status_code == 200
-            except requests.exceptions.ReadTimeout:
+            except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError):
                 return False
 
-        if url_exist(url_item):
+        if check_url(url_item):
             logging.info('Success')
         else:
             logging.error('Failed')
